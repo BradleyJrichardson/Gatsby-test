8974b33877649a97da54fa5e6c52e5fde35ba212
BradleyJrichardson <brdl.richardson@gmail.com>
2019-05-27 08:24:16 +1000

    .

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () => (
+  <div style={{ color: `purple` }}>
+    <h1>Gatsby</h1>
+    <p>Is worth learning</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+)
